287e7b5b36cc993e12a22ab09788ecefa125407c
Salini <shalini021037@gmail.com>
2022-10-21 09:34:04 +0530

    Add project files.

---
+++ CalculatorLibJenkinsDemo/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibJenkinsDemo
+{
+    
+    public class Calculator
+    {
+        
+       
+        public int Add(int x,int y)
+        {
+            return x + y;
+            
+        }
+        public int Subtract(int x,int y)
+        {
+            return x - y;
+            
+        }
+        public int Multiply(int x,int y)
+        {
+            return x * y;
+            
+        }
+    }
+}
